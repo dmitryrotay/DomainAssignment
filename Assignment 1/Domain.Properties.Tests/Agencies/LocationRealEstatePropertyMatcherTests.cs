59da898c09e89a0d7294be1162717d3770080b8e
Dmitry Rotay <dmitry.rotay@gmail.com>
2020-11-17 18:27:33 +1100

    Add missing LRE agency matching criterion

--- Assignment 1/Domain.Properties.Tests/Agencies/LocationRealEstatePropertyMatcherTests.cs
+++ Assignment 1/Domain.Properties.Tests/Agencies/LocationRealEstatePropertyMatcherTests.cs
@@ -9,21 +9,45 @@ namespace Domain.Properties.Tests.Agencies
         private const decimal BaseLatitude = -33.8684401m;
         private const decimal BaseLongitude = 151.192646m;
 
-        private const decimal DegreesInThresholdDistance = (decimal) 1 /
+        private const decimal DegreesInThresholdDistance = (decimal)1 /
                                                            LocationRealEstatePropertyMatcher.MetersInOneDegree *
                                                            LocationRealEstatePropertyMatcher.DistanceThresholdMeters;
 
         [Fact]
-        public void IsMatch_Returns_True_If_Only_Latitude_Changes_And_Latitude_Within_200m()
+        public void IsMatch_Returns_False_If_Coordinates_Equal_And_Agency_Codes_Dont_Match()
         {
             var agencyProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude,
                 Longitude = BaseLongitude
             };
 
             var databaseProperty = new Property
             {
+                AgencyCode = Constants.AgencyCode,
+                Latitude = BaseLatitude,
+                Longitude = BaseLongitude
+            };
+
+            var matcher = new LocationRealEstatePropertyMatcher();
+
+            Assert.False(matcher.IsMatch(agencyProperty, databaseProperty));
+        }
+
+        [Fact]
+        public void IsMatch_Returns_True_If_Only_Latitude_Changes_And_Latitude_Within_Threshold()
+        {
+            var agencyProperty = new Property
+            {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
+                Latitude = BaseLatitude,
+                Longitude = BaseLongitude
+            };
+
+            var databaseProperty = new Property
+            {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude + DegreesInThresholdDistance,
                 Longitude = BaseLongitude
             };
@@ -34,16 +58,18 @@ namespace Domain.Properties.Tests.Agencies
         }
 
         [Fact]
-        public void IsMatch_Returns_False_If_Only_Latitude_Changes_And_Latitude_Not_Within_200m()
+        public void IsMatch_Returns_False_If_Only_Latitude_Changes_And_Latitude_Not_Within_Threshold()
         {
             var agencyProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude,
                 Longitude = BaseLongitude
             };
 
             var databaseProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude + DegreesInThresholdDistance + 1,
                 Longitude = BaseLongitude
             };
@@ -54,16 +80,18 @@ namespace Domain.Properties.Tests.Agencies
         }
 
         [Fact]
-        public void IsMatch_Returns_True_If_Only_Longitude_Changes_And_Latitude_Within_200m()
+        public void IsMatch_Returns_True_If_Only_Longitude_Changes_And_Latitude_Within_Threshold()
         {
             var agencyProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude,
                 Longitude = BaseLongitude
             };
 
             var databaseProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude,
                 Longitude = BaseLongitude + DegreesInThresholdDistance
             };
@@ -74,16 +102,18 @@ namespace Domain.Properties.Tests.Agencies
         }
 
         [Fact]
-        public void IsMatch_Returns_False_If_Only_Longitude_Changes_And_Latitude_Not_Within_200m()
+        public void IsMatch_Returns_False_If_Only_Longitude_Changes_And_Latitude_Not_Within_Threshold()
         {
             var agencyProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude,
                 Longitude = BaseLongitude
             };
 
             var databaseProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude,
                 Longitude = BaseLongitude + DegreesInThresholdDistance + 1
             };
@@ -94,18 +124,20 @@ namespace Domain.Properties.Tests.Agencies
         }
 
         [Fact]
-        public void IsMatch_Returns_True_If_Both_Coordinates_Change_And_Distance_Within_200m()
+        public void IsMatch_Returns_True_If_Both_Coordinates_Change_And_Distance_Within_Threshold()
         {
-            decimal squareSideFromDiagonal = DegreesInThresholdDistance / (decimal) Math.Sqrt(2);
+            decimal squareSideFromDiagonal = DegreesInThresholdDistance / (decimal)Math.Sqrt(2);
 
             var agencyProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude,
                 Longitude = BaseLongitude
             };
 
             var databaseProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude + squareSideFromDiagonal,
                 Longitude = BaseLongitude + squareSideFromDiagonal
             };
@@ -116,18 +148,20 @@ namespace Domain.Properties.Tests.Agencies
         }
 
         [Fact]
-        public void IsMatch_Returns_False_If_Both_Coordinates_Change_And_Distance_Not_Within_200m()
+        public void IsMatch_Returns_False_If_Both_Coordinates_Change_And_Distance_Not_Within_Threshold()
         {
             decimal squareSideFromDiagonal = (DegreesInThresholdDistance + 1) / (decimal)Math.Sqrt(2);
 
             var agencyProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude,
                 Longitude = BaseLongitude
             };
 
             var databaseProperty = new Property
             {
+                AgencyCode = LocationRealEstatePropertyMatcher.LreAgencyCode,
                 Latitude = BaseLatitude + squareSideFromDiagonal,
                 Longitude = BaseLongitude + squareSideFromDiagonal
             };
--- Assignment 1/Domain.Properties/Matching/Agencies/LocationRealEstatePropertyMatcher.cs
+++ Assignment 1/Domain.Properties/Matching/Agencies/LocationRealEstatePropertyMatcher.cs
@@ -6,15 +6,21 @@ namespace Domain.Properties.Matching.Agencies
     {
         public const int MetersInOneDegree = 111000;
         public const int DistanceThresholdMeters = 200;
+        public const string LreAgencyCode = "LRE";
 
-        public string AgencyCode => "LRE";
-        
+        public string AgencyCode => LreAgencyCode;
+
         public bool IsMatch(Property agencyProperty, Property databaseProperty)
         {
+            if (!string.Equals(databaseProperty.AgencyCode, AgencyCode, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
             var latitudeDifferenceMeters = (databaseProperty.Latitude - agencyProperty.Latitude) * MetersInOneDegree;
             var longitudeDifferenceMeters = (databaseProperty.Longitude - agencyProperty.Longitude) * MetersInOneDegree;
-            var distance = Math.Sqrt(Math.Pow((double) latitudeDifferenceMeters, 2) +
-                                     Math.Pow((double) longitudeDifferenceMeters, 2));
+            var distance = Math.Sqrt(Math.Pow((double)latitudeDifferenceMeters, 2) +
+                                     Math.Pow((double)longitudeDifferenceMeters, 2));
 
             return distance <= DistanceThresholdMeters;
         }
